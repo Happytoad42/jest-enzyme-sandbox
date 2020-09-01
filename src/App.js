55697f45b265c19527f6319518c189c819ac8ff3
Михайлов Артём Андреевич <amihaylov@ask.local>
2020-09-01 17:56:37 +0700

    added click app testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  state = {
+    counter: 0,
+  };
+
+  handleIncrement() {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+    }));
+  }
+
   render() {
     return (
       <div data-test="component-app" className="App">
         <h1>Learn react Testing</h1>
+        <h2 data-test="counter-display">
+          Current counter is {this.state.counter}
+        </h2>
+        <button data-test="increment-button" onClick={this.handleIncrement}>
+          Increment
+        </button>
       </div>
     );
   }
